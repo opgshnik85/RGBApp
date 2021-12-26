41ea4f9f35ac50bb11671a04661e505e8a55abf1
Tikhonov Mikhail <84095451+opgshnik85@users.noreply.github.com>
2021-12-26 22:35:26 +0200

    Add files via upload

---
+++ RGBApp/RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by MacBook on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
